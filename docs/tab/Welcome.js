e58521d357de0ecf73a297f10e809234a1697eff
Porshen Lai <porshenlai@gmail.com>
2025-04-07 15:45:35 +0800

    add link to application page

--- docs/tab/Welcome.js
+++ docs/tab/Welcome.js
@@ -1,3 +1,7 @@
 document.currentScript.value=async (root,args)=>{
 	console.log("Script running...");
+	root.querySelector('button[UIE="Start"]').addEventListener('click',function(event){
+		document.body.querySelector('[func="SmartBudget"]').click();
+
+	});
 };
